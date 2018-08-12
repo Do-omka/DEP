2168fe92b1670dace4d156689ead61f5355d81c4
Doomka <do-omer@ya.ru>
2018-08-13 00:00:53 +0400

    fixed slider and something for some reason(???)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+	var btn1 = document.getElementById("btn-1"),
+	btn2 = document.getElementById("btn-2"),
+	btn3 = document.getElementById("btn-3");
+	var nextSlide;
+
+
+	btn1.addEventListener('change', function() {
+		if (btn1.checked === true) {
+			clearTimeout(nextSlide);
+			nextSlide = setTimeout(function() {
+				btn2.checked = true;
+				btn2.dispatchEvent(new Event('change'));
+			}, 2000);
+		}
+	});
+
+	btn2.addEventListener('change', function() {
+		if (btn2.checked === true) {
+			clearTimeout(nextSlide);
+			nextSlide = setTimeout(function() {
+				btn3.checked = true;
+				btn3.dispatchEvent(new Event('change'));
+			}, 2000);
+		}
+	});
+
+	btn3.addEventListener('change', function() {
+		if (btn3.checked === true) {
+			clearTimeout(nextSlide);
+			nextSlide = setTimeout(function() {
+				btn1.checked = true;
+				btn1.dispatchEvent(new Event('change'));
+			}, 2000);
+		}
+	});
+	btn1.dispatchEvent(new Event('change'));
+	btn2.dispatchEvent(new Event('change'));
+	btn3.dispatchEvent(new Event('change'));
+});
+
 var current = 1;
 document.querySelector('.gallery2 .controls .prev').addEventListener('click', function() {
 	if (current == 2) {
