98efa08e3142a72b402f7e9bf35b4072eb1b61b4
Doomka <do-omer@ya.ru>
2018-10-13 08:14:25 +0400

    fixed gulp = compressing+autoprefixes deleted json packages deleted jekyll theme edited gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,52 @@
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
+const
+	gulp = require('gulp')
+	sourcemaps = require('gulp-sourcemaps')
+	postcss = require('gulp-postcss')
+	less = require('gulp-less')
+	imagemin = require('gulp-imagemin')
+	htmlmin = require('gulp-htmlmin')
+	jsmin = require('gulp-uglify')
+	pump = require('pump')
+	// livereload = require('gulp-livereload')
 
-gulp.task('minify-css', () => {
-  return gulp.src('styles/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
+function mincss() {
+	const processors = [
+		prefix = require('autoprefixer'),
+		csso = require('postcss-csso')
+	]
+	return gulp.src('less/*.less')
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.identityMap())
+		.pipe(less())
+		.pipe(postcss(processors))
+		.pipe(sourcemaps.write('', {includeContent: false, sourceRoot: 'less'}))
+		.pipe(gulp.dest('css'))
+		// .pipe(livereload({ start: true }))
+}
+
+gulp.task('minhtml', () => {
+	return gulp.src('dev/*.html')
+	.pipe(htmlmin({ collapseWhitespace: true }))
+	.pipe(gulp.dest('prod'));
 });
 
-gulp.task('watch', function(){
-	gulp.watch('less/*.less', ['default']);
-	gulp.watch('css/*.css', ['default']);
+gulp.task(function watchcss() {
+	gulp.watch('less/*.less', mincss)
+}
+)
+gulp.task('imgmin', () => {
+	return gulp.src('img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('imgs'))
+}
+)
+gulp.task('minjs', function (cb) {
+	pump([
+		gulp.src('js/*.js'),
+		jsmin(),
+		gulp.dest('dist')
+	],
+	cb
+	)
 })
+gulp.task('default', gulp.series(mincss, 'watchcss'))
